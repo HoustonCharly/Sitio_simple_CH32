1005f8456aa1a4de2ecf2722333a63a36d5a1cbd
HoustonCharly <jriverar1801@alumno.ipn.mx>
2023-09-04 14:08:47 -0600

    Primer subida de sitio web CH32

---
+++ javascripts/1javascript.js
@@ -0,0 +1,10 @@
+//Crear constantes
+/*const name = "Juan";
+//Tipos de metodo console
+console.error("Error");
+console.warn("Warning");
+console.info("Info");
+console.log("Hello World! of: "+name);
+*/
+alert("Estoy vivo!!...");
+console.log("Aqui de curioso en el navegadro...");
